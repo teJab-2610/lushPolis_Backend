0fee15bdda9070343018ef0364d0017207de3b58
BetterNut <tejab2610@gmail.com>
2023-11-14 01:23:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ require('./routes/product.route')(app);
 const server = require('http').createServer(app);
   const io = require('socket.io')(server, {
     cors: {
-      origin: [process.env.BASE_URL],
+      origin: ['https://lush-polis-frontend.vercel.app'],
       //methods: ['GET', 'POST']
     }
   })
